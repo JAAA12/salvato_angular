717bc22fe2651bea8c777cff0e6558f6d1dd111a
JAAA12 <juliana.arciniegas@sisloga.com>
2023-01-31 17:44:41 -0500

    intento de hacer funcionar el login segun select

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+ condicion?:boolean;
+ valor(){
+  console.log(this.condicion)
+ }
 }
